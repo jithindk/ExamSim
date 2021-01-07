5b0a301983c2b753bef8b047006cbf24c32fe77e
jithindk <jithinloyolite@gmail.com>
2021-01-08 02:58:51 +0530

    for heroku

--- ExamSim/settings.py
+++ ExamSim/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'izt402gwv6lh^mu++n(zgl%jbuqx@qr6+u%w656y-@wvqx2_n@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exam-assistant.herokuapp.com','127.0.0.1']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ExamSim.wsgi --log-file -
